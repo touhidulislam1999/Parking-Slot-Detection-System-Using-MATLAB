ea986ef76857d64c267f9d46b680d55492e04f3d
touhidulislam1999 <touhidulislam@iut-dhaka.edu>
2024-09-22 03:24:10 +0600

    Add files via upload

---
+++ untitled1.m
@@ -0,0 +1,94 @@
+function varargout = untitled1(varargin)
+%      Making a function for named "untitled1' which is the main function
+%      for our "Car Parking Management System Code"
+%      UNTITLED1 MATLAB code for untitled1.fig
+%      UNTITLED1, by itself, creates a new UNTITLED1 or raises the existing
+%      singleton*.
+%
+%      H = UNTITLED1 returns the handle to a new UNTITLED1 or the handle to
+%      the existing singleton*.
+%
+%      UNTITLED1('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in UNTITLED1.M with the given input arguments.
+%
+%      UNTITLED1('Property','Value',...) creates a new UNTITLED1 or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before untitled1_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to untitled1_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Last Modified by GUIDE v2.5 10-Apr-2020 
+
+% Begin initialization code
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @untitled1_OpeningFcn, ...
+                   'gui_OutputFcn',  @untitled1_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+
+% --- Executes just before untitled1 is made visible.
+function untitled1_OpeningFcn(hObject, ~, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to untitled1 (see VARARGIN)
+
+% Choose default command line output for untitled1
+handles.output = hObject;
+% Update handles structure
+guidata(hObject, handles);
+% UIWAIT makes untitled1 wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+% --- Outputs from this function are returned to the command line.
+function varargout = untitled1_OutputFcn(~, ~, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, ~, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+handles.output=hObject;
+[a, b]=uigetfile({'*.*'});
+img=imread([b a]);
+grayy=rgb2gray(img);
+gr=graythresh(grayy);
+handles.bw=im2bw(grayy,gr);
+imshow(img,'Parent',handles.axes1);
+guidata(hObject,handles);
+
+% --- Executes on button press in pushbutton2.
+function pushbutton2_Callback(hObject, ~, handles)
+% hObject    handle to pushbutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+handles.output=hObject;
+inverse_binary=not(handles.bw);
+[handles.L, handles.Num_object]=bwlabel(inverse_binary);
+set(handles.text2,'string',10-handles.Num_object);
+imshow(handles.L, 'parent',handles.axes2);
+guidata(hObject,handles);
